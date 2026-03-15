9cb6d0bc71d2759aac4f0497b2a82edf946e8f87
2005santhosh <damerasanthosh2005@gmail.com>
2026-03-15 22:55:43 +0530

    updated

--- frontend/js/investorDashboard.js
+++ frontend/js/investorDashboard.js
@@ -4,7 +4,7 @@
 let user = JSON.parse(localStorage.getItem('user') || '{}');
 const token = localStorage.getItem('token');
 const userId = (user._id || user.id)?.toString();
-// const API_URL = "https://api.dolphinorg.in/api";
+const API_URL = "https://api.dolphinorg.in/api";
 
 if (!token) window.location.href = 'login.html';
 
